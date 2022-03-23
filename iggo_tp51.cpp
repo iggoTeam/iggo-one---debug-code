d360b7594382f3e96264deb707f7e3cd1a818b86
iggo team <102258380+iggoTeam@users.noreply.github.com>
2022-03-23 22:00:04 +0100

    Create iggo_tp51.cpp

---
+++ iggo_tp51.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "iggo_tp51.h"
+#define POWER PD5
+
+iggo_tp51 tp51;
+
+void setup()
+{
+  Serial.begin(250000);
+  pinMode(POWER, OUTPUT);
+  digitalWrite(POWER, 1);
+    Serial.println("iggo_tp51 Arduino Test");
+    tp51.begin(0x40); // default I2C address is 0x40 and 14-bit measurement resolution
+    Serial.print("firmware revision: ");
+    switch (tp51.readFirmwareVersion())
+    {
+    case 0xFF:
+        Serial.println("version 1.0");
+        break;
+    case 0x20:
+        Serial.println("version 2.0");
+        break;
+    default:
+        Serial.println("unknow");
+            break;
+    }
+    p51.setResolution(14) // where num = 14,12,13,11-bits, default = 14
+}
+void loop()
+{
+    Serial.print("T=");
+    Serial.print(tp51.readTemperature());
+    Serial.println("°C");
+    delay(300);
+}
